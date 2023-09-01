bf31fdd44ceaf8b27df56e1b26a97202bb089ae3
zeroknots <zeroknots@protonmail.com>
2023-09-01 10:31:33 +0700

    adding tests

--- test/SentinelListTest.t.sol
+++ test/SentinelListTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 
 import { SentinelListLib } from "../src/SentinelList.sol";
-import "../src/Helper.sol";
+import "../src/SentinelListHelper.sol";
 
 /// @author zeroknots
 contract SentinelListTest is Test {
@@ -16,9 +16,41 @@ contract SentinelListTest is Test {
         list.init();
     }
 
+    function testAddMany() public {
+        address addr1 = makeAddr("1");
+        address addr2 = makeAddr("2");
+        address addr3 = makeAddr("3");
+        address addr4 = makeAddr("4");
+        address addr5 = makeAddr("5");
+        address addr6 = makeAddr("6");
+        address addr7 = makeAddr("7");
+        address addr8 = makeAddr("8");
+
+        list.push(addr2);
+        list.push(addr4);
+        list.push(addr7);
+        list.push(addr5);
+        list.push(addr1);
+        list.push(addr6);
+        list.push(addr3);
+        list.push(addr8);
+
+        assertTrue(list.contains(addr1));
+        assertTrue(list.contains(addr2));
+        assertTrue(list.contains(addr3));
+        assertTrue(list.contains(addr4));
+        assertTrue(list.contains(addr5));
+        assertTrue(list.contains(addr6));
+        assertTrue(list.contains(addr7));
+        assertTrue(list.contains(addr8));
+
+        assertFalse(list.contains(makeAddr("9")));
+    }
+
     function testAdd() public {
         address addr1 = makeAddr("1");
         address addr2 = makeAddr("2");
+
         list.push(addr2);
         assertFalse(list.contains(addr1));
         assertTrue(list.contains(addr2));
